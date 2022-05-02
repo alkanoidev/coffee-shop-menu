4f876f596eba3ed07a4899f5960301e81b335aff
Filip Ivanovic <alkanoidev@gmail.com>
2022-05-02 19:49:40 +0200

    added getCategory function

--- client/src/components/Buttons/Button.js
+++ client/src/components/Buttons/Button.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-export default function Button({title, onClick}) {
+export default function Button({ title, onClick }) {
   return (
     <button
-      type="button"
       className="text-white bg-brown focus:ring-4 focus:ring-amber-900 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
       onClick={onClick}
     >
--- client/src/pages/NewItem.js
+++ client/src/pages/NewItem.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../components/Layout";
 import Button from "../components/Buttons/Button";
+import axios from "axios";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 export default function NewItem() {
+  const [item, setItem] = useState({
+    name: "",
+    price: null,
+    description: "",
+    category: "",
+  });
+  const [categoryId, setCategoryId] = useState("");
+
+  const getCategoryId = async () => {
+    await axios
+      .get(`http://localhost:3001/categories/category/${item.category}`)
+      .then((res) => {
+        setCategoryId(res.data.category._id);
+        console.log(categoryId);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getCategoryId();
+    // axios
+    //   .post(`http://localhost:3001/items/item/new/${categoryId}`, item)
+    //   .then(function (response) {
+    //     Navigate("/");
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setItem((prev) => ({ ...prev, [name]: value }));
+    console.log(item);
+  };
+
   return (
     <Layout>
       <div className="flex flex-col items-center">
-        <div className="p-1 w-full sm:w-3/4 md:w-2/4 lg:w-2/4 xl:w-2/6 2xl:w-1/6 flex flex-col gap-4">
+        <form className="p-1 w-full sm:w-3/4 md:w-2/4 lg:w-2/4 xl:w-2/6 2xl:w-1/6 flex flex-col gap-4">
           <div>
             <label
               htmlFor="first_name"
@@ -18,7 +57,11 @@ export default function NewItem() {
               type="text"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
               placeholder="John"
+              name="name"
               required
+              onChange={(e) => {
+                handleChange(e);
+              }}
             />
           </div>
           <div>
@@ -33,6 +76,10 @@ export default function NewItem() {
               rows="4"
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
               placeholder="Your message..."
+              name="description"
+              onChange={(e) => {
+                handleChange(e);
+              }}
             ></textarea>
           </div>
           <div>
@@ -47,6 +94,10 @@ export default function NewItem() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
               placeholder="John"
               required
+              name="price"
+              onChange={(e) => {
+                handleChange(e);
+              }}
             />
           </div>
           <div>
@@ -61,10 +112,14 @@ export default function NewItem() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
               placeholder="John"
               required
+              name="category"
+              onChange={(e) => {
+                handleChange(e);
+              }}
             />
           </div>
-          <Button title="Add Item" />
-        </div>
+          <Button title="Submit Item" onClick={handleSubmit} />
+        </form>
       </div>
     </Layout>
   );
