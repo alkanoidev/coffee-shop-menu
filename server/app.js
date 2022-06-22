124f1672c3b2d3578f93b9aa3c9ed9d92d1dce38
Filip Ivanovic <alkanoidev@gmail.com>
2022-06-23 00:43:50 +0200

    dsfs

--- server/app.js
+++ server/app.js
@@ -5,8 +5,9 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
+const compression = require("compression")
+const helmet = require("helmet")
 require("dotenv").config();
-const serverless = require("serverless-http");
 
 const itemRouter = require("./routes/itemRouter");
 const categoryRouter = require("./routes/categoryRouter");
@@ -20,9 +21,10 @@ const database = client.db("inventoryapplication");
 module.exports.database = database;
 
 const app = express();
-const router = express.Router();
 
 app.use(logger("dev"));
+app.use(compression());
+app.use(helmet())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -44,13 +46,12 @@ app.use(function (err, req, res, next) {
 // app.use(express.static(path.join(__dirname, "public")));
 // app.use(express.static(path.join(__dirname, "client", "build"))); // production
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.redirect("/items");
 });
 
 app.use("/items", itemRouter);
 app.use("/categories", categoryRouter);
-app.use("/.netlify/functions/api", router);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${port}`);
